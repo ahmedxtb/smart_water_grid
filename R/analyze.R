3107702147b70301467b07568cd42cae1566b699
dljvandenberg <dljvandenberg@gmail.com>
2016-01-21 14:22:13 +0100

    Initial exploratory code

---
+++ R/analyze.R
@@ -0,0 +1,37 @@
+### Analyze Water Quality Measurements
+
+
+
+## PREPARE
+
+# Print session info
+sessionInfo()
+
+# Libraries
+library(dplyr)
+library(reshape2)
+library(lubridate)
+library(ggplot2)
+library(caret)
+
+# Variables
+dirseparator <- "/"
+dir.wd <- "~/git/smart_water_grid"
+dir.input <- paste(dir.wd, "data", sep=dirseparator)
+dir.output <- paste(dir.wd, "results", sep=dirseparator)
+url.data <- "https://db.tt/wf2nir4S"
+filepath.data <- paste(dir.input, "data.zip", sep=dirseparator)
+
+# Workdir
+setwd(dir.wd)
+
+
+## IMPORT
+
+
+## CLEAN
+
+
+## EXPLORE
+
+
