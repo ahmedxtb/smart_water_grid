782a8213d082f03003c55c325468ad2be0adff6e
dljvandenberg <dljvandenberg@gmail.com>
2016-01-23 22:40:07 +0100

    Outline of analysis

---
+++ R/analyze.R
@@ -0,0 +1,42 @@
+### Analyze Water Quality Data
+
+
+## List of files for each category (eventlab, temperature, etc)
+
+
+
+## read.csv (only numeric data) -> dataset for each category (ignore 'other' vars)
+
+## Attach variables vertically
+
+## Eventlab vars: convert from numeric to factor (normal/alarm)
+
+## Aggregate per hour
+
+
+
+## Heatmaps (al least for eventlab, possibly for other categories)
+
+## Dendrogram (related vars within categories)
+
+## Multivariate plots (for multiple categories)
+
+
+## Select 1 response var
+
+## Select 1 interesting dataset with predictors (for example turbidity)
+
+## Select interesting time range (maybe not necessary)
+
+## Merge predictor set and response var
+
+## Remove rows with NA (consider these as clean datasets that we can work with)
+
+
+## Split into train and test sets
+
+## Apply ML algorithm (RF, GBM, Tree)
+
+## Predict and validate
+
+## Check list of important predictors
